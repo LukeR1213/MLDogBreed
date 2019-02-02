f60cc90389aec23aa4e1df26277990ab802b50b2
JadenBanson <jaden.banson@gmail.com>
2019-02-02 01:03:27 -0500

    initial iOS commit
    - Adding xCode

---
+++ Dog Breeds/Dog Breeds/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Dog Breeds
+//
+//  Created by Jaden Banson on 2019-02-02.
+//  Copyright © 2019 Jaden Banson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.blue
+    }
+
+
+}
+
